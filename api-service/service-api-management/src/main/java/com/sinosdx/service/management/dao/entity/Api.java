f7f9ee9feba73a7afc42093a89a53e8394d30f7e
songmingsong <mingsong.song@sinosdx.com>
2021-12-09 17:57:55 +0800

    修改 port

--- api-service/service-api-management/src/main/java/com/sinosdx/service/management/dao/entity/Api.java
+++ api-service/service-api-management/src/main/java/com/sinosdx/service/management/dao/entity/Api.java
@@ -46,7 +46,6 @@ public class Api extends Entity<Integer> {
         this.description = apiVo.getDescription();
         this.markdown = apiVo.getMarkdown();
         this.domain = apiVo.getDomain().startsWith("http") ? apiVo.getDomain() + ":" + apiVo.getPort() : apiVo.getProtocol() + "://" + apiVo.getDomain() + ":" + apiVo.getPort();
-        this.domain = apiVo.getDomain().startsWith("http") ? apiVo.getDomain() : apiVo.getProtocol() + "://" + apiVo.getDomain();
         this.url = apiVo.getApiUrl();
         this.prefixPath = apiVo.getUpstreamPrefixPath() + apiVo.getPrefixPath();
         this.requestMethod = apiVo.getRequestMethod();
