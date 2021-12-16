79994c610fa454c619a6a01ec2adc3fc2a39e54d
hechuanzhen <chuanzhen.he@sinosdx.com>
2021-12-16 13:31:01 +0800

    前端初始化

--- sinosdx-website/src/api/apiTemplate.js
+++
@@ -1,7 +0,0 @@
-import request from '@/utils/request'
-export function templateList(name) {
-  return request({
-    url: '/app/api/template/list' + name,
-    method: 'get'
-  })
-}
--- sinosdx-website/src/api/dashboard.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function dashboard() {
-  return request({
-    url: '/app/dashboard',
-    method: 'get',
-  })
-}
