e9d59f87967a968dd298c5e98dbf90735de9114d
songmingsong <mingsong.song@sinosdx.com>
2021-12-23 15:40:46 +0800

    1.添加ErrorLogFilter 和 HttpLogFilter

---
+++ sinoapix-gateway/gateway-plugin/src/main/java/com/sinosdx/common/gateway/plugin/filter/custom/ErrorLogGatewayFilterFactory.java
@@ -0,0 +1,59 @@
+package com.sinosdx.common.gateway.plugin.filter.custom;
+
+
+import cn.hutool.core.date.DateTime;
+import com.sinosdx.common.base.result.R;
+import com.sinosdx.common.gateway.entity.BaseConfig;
+import com.sinosdx.common.gateway.plugin.enums.FilterResultCodeEnum;
+import com.sinosdx.common.gateway.plugin.filter.BaseGatewayFilter;
+import com.sinosdx.common.gateway.plugin.utils.HttpUtil;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 防重放攻击
+ *
+ * @author pengjiahu
+ * @date 2021-06-18 00:43
+ * @description
+ */
+@Slf4j
+@Component
+public class ErrorLogGatewayFilterFactory extends BaseGatewayFilter<ErrorLogGatewayFilterFactory.Config> {
+
+    public ErrorLogGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public Mono<Void> customApply(ServerWebExchange exchange, GatewayFilterChain chain, Config c) {
+        ServerHttpRequest req = exchange.getRequest();
+        return chain.filter(exchange);
+    }
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    @ToString(callSuper = true)
+    public static class Config extends BaseConfig {
+
+        private String ip;
+
+    }
+
+}
