f5fe01456c87a29cd401ef01a7530a16cafbe5a3
彭加虎 <jiahu.peng@sinosdx.com>
2022-01-11 10:31:46 +0800

    [ADD] deploy-增加基础环境部署脚本

---
+++ docs/deploy/env/install-git.sh
@@ -0,0 +1,33 @@
+#! /bin/shell
+
+#======================================================================
+# 快速安装GIT
+# CentOS7 中已成功验证
+# 使用yum方式安装
+#======================================================================
+
+hasGit(){
+    GIT_VERSION=$(git --version)
+    echo "${GIT_VERSION}"
+    if [[ $GIT_VERSION == *version* ]]
+    then
+        return 1;
+    fi
+    return 0;
+}
+
+hasGit
+
+if [ $? != 1 ]
+then
+    echo "Not Found git"
+    echo "Installing Git..."
+    yum install -y git
+    hasGit
+    if [ $? != 1 ]
+    then
+      echo "Install maven Fail"
+    fi
+fi
+
+echo ""
---
+++ docs/deploy/env/install-jdk.sh
@@ -0,0 +1,36 @@
+#! /bin/shell
+
+#======================================================================
+# 快速安装JDK
+# CentOS7 中已成功验证
+# 使用yum方式安装
+#======================================================================
+
+
+hasJdk(){
+    RESULT=$(pgrep java)
+    if [[ ! $RESULT ]]
+    then
+        return 0;
+    fi
+    return 1;
+}
+
+hasJdk
+if [ $? != 1 ]
+then
+    echo "Not Found jdk"
+    echo "Installing jdk..."
+    yum install -y java-1.8.0-openjdk
+    hasJdk
+    if [ $? != 1 ]
+    then
+      echo "Install jdk Fail"
+    fi
+fi
+
+java -version
+echo ""
+
+
+
