9548d6e4ccf950b3bb88b5746e46cfe9c65d1464
彭加虎 <jiahu.peng@sinosdx.com>
2022-02-11 16:18:23 +0800

    [REM] sql文件敏感信息去除

--- docs/mysql/apix_user.sql
+++ docs/mysql/apix_user.sql
@@ -1,10 +1,8 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 47.103.109.225（sino apix）
  Source Server Type    : MySQL
  Source Server Version : 50736
- Source Host           : 47.103.109.225:30306
  Source Schema         : apix_user
 
  Target Server Type    : MySQL
